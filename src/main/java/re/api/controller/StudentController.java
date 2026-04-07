7768ff60de0d41347d7e205fb62ecd6ba0568ee9
hunghx <baby10051999@gmail.com>
2026-04-07 21:06:10 +0700

    buoi7

--- src/main/java/re/api/controller/StudentController.java
+++ src/main/java/re/api/controller/StudentController.java
@@ -1,21 +1,25 @@
 package re.api.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import re.api.dto.request.StudentAdd;
 import re.api.dto.response.StudentResponse;
 import re.api.entity.Student;
 import re.api.service.IStudentService;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/students")
 @RequiredArgsConstructor
 public class StudentController {
-    private IStudentService studentService;
+    private final IStudentService studentService;
     // Data binding : liên kết dữ liệu
     // Xử lí được xác thực dữ liệu
 
@@ -34,7 +38,17 @@ public class StudentController {
     }
     // Thêm mới
     @PostMapping
-    public ResponseEntity<StudentResponse> addStudent(@RequestBody StudentAdd request) {
+    // Để kích hoạt cơ chế validate chúng ta sử dụng @Valid
+    public ResponseEntity<?> addStudent(@Valid @RequestBody StudentAdd request, BindingResult result) {
+        if (result.hasErrors()){
+            // nếu có lỗi thì làm gì
+            Map<String, String> errors = new HashMap<>(); // danh sách lỗi
+            result.getFieldErrors().forEach(err->{
+                errors.put(err.getField(), err.getDefaultMessage());
+            });
+            return ResponseEntity.badRequest().body(errors); // 400
+        }
+
         return new  ResponseEntity<>(studentService.addStudent(request), HttpStatus.CREATED); // 201
     }
     // Cập nhật
--- src/main/java/re/api/dto/request/StudentAdd.java
+++ src/main/java/re/api/dto/request/StudentAdd.java
@@ -1,5 +1,9 @@
 package re.api.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 import re.api.dto.Sex;
@@ -7,11 +11,23 @@ import re.api.dto.Sex;
 import java.time.LocalDate;
 @Getter@Setter
 public class StudentAdd {
+    @NotNull
+    // độ dài ID phải đủ 10 k tự, bắt buộc bắt đầu bằng SV và 8 kí tự số
+    @Pattern(regexp ="^SV[0-9]{8}$")
+
     private String id;
+    @NotBlank(message = "Không để trống")
     private String name;
+    @NotNull
+    @Email
     private String email;
+    @NotNull
     private LocalDate dateOfBirth;
     private String address;
+    @NotNull
+    // s điện thoại bắt đầu bằng số 0 và có 10 kí tự số
+    @Pattern(regexp = "^0[0-9]{9}$")
     private String phone;
+    @NotNull
     private Sex sex;
 }
